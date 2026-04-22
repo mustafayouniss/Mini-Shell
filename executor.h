d3eaca4572aba648cc3ee0d98289ce6814b50e66
Maram Ahmed <marmora000mm@gmail.com>
2026-04-22 18:13:34 +0200

    Completed background execution task

--- executor.h
+++ executor.h
@@ -1,13 +1,14 @@
 #ifndef EXECUTOR_H
 #define EXECUTOR_H
+#include "signals.h"
 /*
  * executor.h
  * Public interface for executor.c
 */
 
 #include "parser.h"
-
-void apply_redir(Command *cmd);
+int execute_pipeline(Command *head);
 int execute_command(Command *cmd);
+void apply_redir(Command *cmd);
 
 #endif /* EXECUTOR_H */
